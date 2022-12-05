930a5153319b98ed7dcb1c994c84b3069d358484
Hans-Christian Otto <c.otto@suora.com>
2022-12-05 14:39:19 +0100

    reformat code :D

--- src/typescript/formatNameExample.tsx
+++ src/typescript/formatNameExample.tsx
@@ -1,24 +1,23 @@
 interface User {
-    firstName: string;
-    lastName: string;
+  firstName: string;
+  lastName: string;
 }
 
-
 const something = ["a", "b", "c"];
-const something2 = {a: "a", "b":"b", "c": "c"};
+const something2 = { a: "a", b: "b", c: "c" };
 
 formatName({
-    firstName: "bla",
-    lastName: "blub"
-})
+  firstName: "bla",
+  lastName: "blub",
+});
 
 function formatName(user: User) {
-    return `${user.firstName} ${user.lastName}`;
+  return `${user.firstName} ${user.lastName}`;
 }
 
 const user: User = {
-    firstName: 'Miriam',
-    lastName: 'Janssen',
+  firstName: "Miriam",
+  lastName: "Janssen",
 };
 
-const element = <h1>👋, {formatName(user)}!</h1>;
+export const element = <h1>👋, {formatName(user)}!</h1>;
