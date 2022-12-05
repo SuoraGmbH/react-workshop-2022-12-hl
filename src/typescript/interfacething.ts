a4016a2614f86e82c0f79478d513b2f60f894420
Hans-Christian Otto <c.otto@suora.com>
2022-12-05 15:08:28 +0100

    show interface declaration merging

---
+++ src/typescript/interfacething.ts
@@ -0,0 +1,14 @@
+export const bla = "";
+
+interface User {
+  firstName: string;
+}
+
+interface User {
+  lastName: string;
+}
+
+const user: User = {
+  firstName: "asdf",
+  lastName: "asdf",
+};
