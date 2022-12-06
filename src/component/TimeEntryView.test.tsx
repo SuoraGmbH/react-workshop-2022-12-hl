ee97509b874aa58cc420d8ac6c7c84cef8ef3ed7
Hans-Christian Otto <c.otto@suora.com>
2022-12-06 16:37:42 +0100

    Commit smoke test

---
+++ src/component/TimeEntryView.test.tsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react";
+import TimeEntryView from "./TimeEntryView";
+import { TimeEntry } from "../domain/TimeEntry";
+
+const timeEntry: TimeEntry = {
+  comment: "React Training",
+  id: "husd9auhdaso8jdu80duj08",
+  start: new Date(),
+  end: new Date(),
+};
+
+describe("<TimeEntryView />", () => {
+  test("Smoke Test", () => {
+    render(<TimeEntryView timeEntry={timeEntry} />);
+  });
+});
