14fa5173996fc9b27602fb9c255eea5f6d8d35a9
Hans-Christian Otto <c.otto@suora.com>
2022-12-06 16:26:12 +0100

    Add map example

---
+++ src/typescript/map.ts
@@ -0,0 +1,39 @@
+export const list: number[] = [1, 2, 3, 4, 5];
+// const doubleNumber = (number) => {
+//   return number * 2;
+// };
+// const formatter = new Intl.NumberFormat("de-DE", {
+//   style: "currency",
+//   currency: "EUR",
+// });
+//
+// const formatCurrency = (number) => formatter.format(number);
+//
+// const list2: string[] = list
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(doubleNumber)
+//   .map(formatCurrency);
